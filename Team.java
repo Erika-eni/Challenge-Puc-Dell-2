4c9acb5cdc6ae6dedc823a7f2c8f530d5adca862
Erika-eni <erikaafedrigo@gmail.com>
2024-08-06 15:19:17 -0300

    Team
    attributes, counter, getters and setters and methods to do "blot", "plif", "advrungh" and "grusht", as well as counting them to later show in the final winner

--- Team.java
+++ Team.java
@@ -1,4 +1,50 @@
 package Game;
 
 public class Team {
+    private String name;
+    private String warCry;
+    private int yearFoundation;
+    private int points;
+    private int blots;
+    private int plifs;
+    private int advrunghs;
+    private int grushts;
+
+    public Team(String name, String warCry, int yearFoundation) {
+        this.name = name;
+        this.warCry = warCry;
+        this.yearFoundation = yearFoundation;
+        this.points = 50;
+        this.blots = 0;
+        this.plifs = 0;
+        this.advrunghs = 0;
+        this.grushts = 0;
+    }
+
+    public String getName() {return name;}
+    public String getWarCry() {return warCry;}
+    public int getYearFoundation() {return yearFoundation;}
+    public int getBlots() {return blots;}
+    public int getPoints() {return points;}
+    public int getPlifs() {return plifs;}
+    public int getAdvrunghs() {return advrunghs;}
+    public int getGrushts() {return grushts;}
+
+    public void doBlot() {
+        points += 5;
+        blots++;
+    }
+    public void doPlif() {
+        points -= 1;
+        plifs++;
+    }
+    public void doAdvrungh() {
+        points -= 10;
+        advrunghs++;
+    }
+    public void grushtPoints() {
+        points += 3;
+        grushts++;
+    }
+
 }
